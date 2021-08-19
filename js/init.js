91ff5ac8a62ca80d6212a8177656e5729f11f961
alexismanghini <amanghini@hotmail.com>
2021-08-19 20:04:44 -0300

    login definitivo
    login completo antes de index

--- js/init.js
+++ js/init.js
@@ -7,8 +7,9 @@ const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/pro
 const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 
-
-window.location.href = "login.html"
+if ((localStorage.getItem("usuario") === null || localStorage.getItem("contrasena") === null) && location.href.indexOf("login.html") ==-1) {
+  location.href = "login.html";
+}
 
 var showSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "block";
