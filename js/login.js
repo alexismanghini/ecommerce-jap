b3c99a120bf457d1ee198f0f5fcae00b870042b2
alexismanghini <amanghini@hotmail.com>
2021-08-19 15:06:37 -0300

    login
    segundo intento login

--- js/login.js
+++ js/login.js
@@ -1,5 +1,16 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+var ingreso = function (user, pass) {
+    if (user.trim() === "" || pass.trim() === "") {
+        alert("Deben rellenarse los campos");
+    } else {
+        localStorage.setItem("usuario", user.trim());
+        localStorage.setItem("contraseña", pass.trim());
+  
+        location.href = "index.html";
+    }
+  }
+
 document.addEventListener("DOMContentLoaded", function (e) {
 });
