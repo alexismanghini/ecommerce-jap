ea8e522a9af03158ddcdf19304a83eef15a2c033
alexismanghini <amanghini@hotmail.com>
2021-10-07 10:14:11 -0300

    carrito
    primer esqueleto

--- js/cart.js
+++ js/cart.js
@@ -3,4 +3,32 @@
 //elementos HTML presentes.
 
 document.addEventListener("DOMContentLoaded", function(e){
+
+    async function getCartProducts(url){
+        let articles = await getJSONData(url);
+        let htmlContentToAppend = "";
+
+        for (product in product.articles) {
+        let cartproduct = articles.data[i];
+
+        htmlContentToAppend =`
+
+            <div class="row">
+                <div class="col-3">
+                    <img src="${cartproduct.imgSrc}" alt="" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">${cartproduct.name}</h4>
+                        <small class="text-muted">${cartproduct.count}</small>
+                    </div>
+                    <p class="mb-1">${cartproduct.unitcost} + ${cartproduct.currency}</p>
+                </div>
+            </div>
+        </a>
+        `
+        document.getElementById("CartProducts").innerHTML = htmlContentToAppend;
+    }
+}
+getCartProducts(CART_INFO_URL2P);
 });
