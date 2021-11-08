990277a272f502ab7088ec786ae631cec69b2534
alexismanghini <amanghini@hotmail.com>
2021-11-08 06:51:46 -0300

    entrega 5
    otro intento fallido no me rindo

--- js/cart.js
+++ js/cart.js
@@ -5,7 +5,6 @@ const premium = document.getElementById("premiumsell")
 const tarjeta = document.getElementById("creditcard")
 const transf = document.getElementById("banktransfer")
 
-
 function ValidarCarrito(e) {
     e.preventDefault();
     if ((standard.checked || gold.checked || premium.checked) && (tarjeta.checked || transf.checked )){
@@ -17,43 +16,45 @@ function ValidarCarrito(e) {
 }
 
 
-document.addEventListener("DOMContentLoaded", function (e) {
 
-    async function getCartArticles(url) {
-        let cartarticles = await getJSONData(url);
-        let htmlContentToAppend = "";
-        console.log("productos", cartarticles);
-
-        cartarticles.data.articles.forEach((onecartarticle) => {
-            let cartproduct = onecartarticle
-
-
-
-            htmlContentToAppend += `
-
-            <div class="card" style="width: 18rem;">
-               <div class="card-body">
-               <h5 class="card-title">${cartproduct.name}</h5>
-               <p class="card-text">${cartproduct.currency} ${cartproduct.unitCost} x Unidad</p>
-               Cantidad <input type="number" min="1" class="livecount" value="${cartproduct.count}">
-               <p class="card-text" id="subtotalprod"> Subtotal Producto($UYU): ${cartproduct.count * cartproduct.unitCost}</p>
-               </div>
-            </div>
-        `
-        })
-        document.getElementById("CartProducts").innerHTML = htmlContentToAppend;
-    }
-
-
-    getCartArticles(CART_INFO_URL2P);
-
-    document.getElementsByClassName("livecount").addEventListener("change", function () {
-
-        let subtotalproducto = document.getElementById("subtotalprod")
-        console.log("subtotal", subtotalproducto)
-
-        subtotalproducto = cartproduct.unitCost * cartproduct.count;
+function ReloadCart() {
+    const elementoscarrito = [...document.getElementsByClassName("card")]
+    let subtotal = 0;
+    elementoscarrito.map(elemento => {
+        const precioUnitario = parseInt(elemento.getElementsByClassName("unitcost")[0].innerHTML)
+        const cantidad = elemento.getElementsByClassName("livecount")[0].value
+        const subtotalElemento = precioUnitario * cantidad
+        elemento.getElementsByClassName("subtotalprod")[0].innerHTML = subtotalElemento
+        subtotal += subtotalElemento;
     })
-    console.log(livecountproduct);
+    document.getElementById("subtotalCarrito").innerHTML = parseInt(subtotal)
 
+}
+
+async function CargarCartArticles(url) {
+    let cartarticles = await getJSONData(url);
+    let htmlContentToAppend = "";
+
+    cartarticles.data.articles.forEach((onecartarticle) => {
+        let cartproduct = onecartarticle
+
+        htmlContentToAppend += `
+
+        <div class="card" style="width: 18rem;">
+           <div class="card-body">
+           <h5 class="card-title">${cartproduct.name}</h5>
+           <p class="card-text">${cartproduct.currency} <number class="unitcost">${cartproduct.unitCost}</number> x Unidad</p>
+           Cantidad <input type="number" min="1" class="livecount" value="${cartproduct.count}">
+           <p class="card-text"> Subtotal Producto($UYU):<number class="subtotalprod">${cartproduct.count * cartproduct.unitCost}</number></p>
+           </div>
+        </div>
+    `
+    })
+    document.getElementById("CartProducts").innerHTML = htmlContentToAppend;
+    ReloadCart();
+}
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    CargarCartArticles(CART_INFO_URL2P);
 });
