850c31fa0eb110adca17bb07e707d3f51e3f17b0
alexismanghini <amanghini@hotmail.com>
2021-11-11 16:59:33 -0300

    entrega 6 intento
    probando hasta que salga

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,17 +1,10 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-function ShowMyProfileTitle() {
-  let htmlContentToAppend = `
-    <h1 class=text-center>${localStorage.getItem("usuario")}</h1>`
-
-  document.getElementById("profilenametitle").innerHTML = htmlContentToAppend;
-}
-
 function EditProfileData() {
   let htmlprofileContentToAppend = `
     
-    <form class="text-center p-4">
+    <form class="text-center p-4" id="ProfileForm">
       <div class="form-group row">
         <label class="col-sm-2 col-form-label">Nombre Completo:</label>
         <div class="col-sm-3">
@@ -21,11 +14,11 @@ function EditProfileData() {
       <div class="form-group row">
         <label class="col-sm-2 col-form-label">Edad:</label>
         <div class="col-sm-1">
-          <input type="number" class="form-control" id="Age" min="0">
+          <input type="text" class="form-control" id="Age">
         </div>
       </div>
       <div class="form-group row">
-        <label class="col-sm-2 col-form-label">email:</label>
+        <label class="col-sm-2 col-form-label">Email:</label>
         <div class="col-sm-3">
           <input type="text" class="form-control" id="eMail" placeholder="example@gmail.com">
         </div>
@@ -35,20 +28,40 @@ function EditProfileData() {
         <div class="col-sm-2">
           <input type="text" class="form-control" id="Telefono">
         </div>
-        </form>
-        <div id="ConfirmNewData">
-        <button type="button" class="btn btn-outline-success">Confirmar</button>
-        </div>`
+        <div>
+        <button type="submit" class="btn btn-outline-success" id="ConfirmNewData">Confirmar</button>
+        </div>
+        </form>`
 
   document.getElementById("profileData").innerHTML = htmlprofileContentToAppend;
 };
 
+function SaveUserData() {
+  let usernombres = document.getElementById("Names").value;
+  let useredad = document.getElementById("Age").value;
+  let useremail = document.getElementById("eMail").value;
+  let usertel = document.getElementById("Telefono").value;
 
-document.getElementById("ModifyProfile").addEventListener("click", function(){
- EditProfileData();
+  let UserSavedProfile = {
+    nombre: usernombres,
+    edad: useredad,
+    email: useremail,
+    tel: usertel
+  }
+
+  localStorage.setItem("UserProfile",JSON.stringify(UserSavedProfile));
+}
+
+
+document.getElementById("ModifyProfile").addEventListener("click", function () {
+  EditProfileData();
 })
 
 
+document.getElementById("ConfirmNewData").addEventListener("click", function (e) {
+  e.preventDefault();
+  SaveUserData();
+})
+
 document.addEventListener("DOMContentLoaded", function (e) {
-  ShowMyProfileTitle();
 });
